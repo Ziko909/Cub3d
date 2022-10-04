90b2384a732c2ec2e0d19368a8cf2e2ff632b50d
Ziko909 <zikoziko078@gmail.com>
2022-10-04 05:25:59 +0100

    feat : check the map file errors

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,5 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 05:20:15 by zaabou            #+#    #+#             */
+/*   Updated: 2022/10/04 05:20:16 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
-#define CUB3D_H
-#include <libc.h>
+# define CUB3D_H
+# include <libc.h>
+# include <errno.h>
+
+// Error Function
+void	ft_put_error(char *error_msg);
+// Parsing Functions
+char	**parsing(char *map_file);
 
 #endif
--- src/cub3D.c
+++ src/cub3D.c
@@ -1,7 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 05:20:47 by zaabou            #+#    #+#             */
+/*   Updated: 2022/10/04 05:23:27 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <cub3d.h>
 
-int main(void)
+void	ft_put_error(char *error_msg)
 {
-	printf("Cub3D in Progress ...\n");
+	int	stdo;
+
+	stdo = dup(1);
+	dup2(STDERR_FILENO, STDOUT_FILENO);
+	printf("\x1b[31m Error : Cub3d : %s\x1b[0m\n", error_msg);
+	dup2(stdo, 1);
+	close(stdo);
+	exit(1);
+}
+
+char	**parsing(char *map_file)
+{
+	int	map_fd;
+
+	map_fd = open(map_file, O_RDONLY, 00444);
+	if (map_fd == -1)
+		ft_put_error(strerror(errno));
+	return (NULL);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		parsing(av[1]);
+	}
 	return (0);
 }
