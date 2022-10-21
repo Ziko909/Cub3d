6334bed2d2de8b7d20ae396d3c4171279b8917a4
Rel-hach <rachidelhachimi6@gmail.com>
2022-10-21 01:39:57 +0100

    refactor: add functions prototype

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 05:20:15 by zaabou            #+#    #+#             */
-/*   Updated: 2022/10/13 02:25:54 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/10/20 06:00:14 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,21 @@
 # include <errno.h>
 # include <stdbool.h>
 # include "../lib/libft/libft.h"
-
-typedef enum e_type
-{
-    textures,
-    nord,
-    south,
-    west,
-    east,
-    floor,
-    Ceilling,
-    map_line
-} t_type;
+# include <mlx.h>
 
 typedef struct s_game
 {
     char            *line;
-    t_type          type;
     struct s_game   *next;
 } t_game;
 
 typedef struct s_var
 {
     struct s_game   *head;
+    void            *ptr_mlx;
+    void            *ptr_win;
+    void            *ptr_img;
+    char            *ptr_img_data;
     char            **map;
     char            *path_north;
     char            *path_south;
@@ -49,6 +41,8 @@ typedef struct s_var
     int             pos_x;
     int             pos_y;
     int             nb_player;
+    int             map_lines;
+    int             large_line;
 } t_var;
 
 
@@ -62,6 +56,7 @@ void    ft_lst_add_to_back(t_game **head, t_game *node);
 t_game  *hold_map_elements(int fd);
 void	ft_free_lists(t_game *head);
 void	free_tdstr(char **str);
+int	    ft_listsize(t_game *lst);
 
 // Parsing Functions :
 // starting the process :
@@ -87,11 +82,13 @@ int		ft_strisnum(char *str);
 
 void    ft_check_store_rgb(t_var *g, char *str, char c);
 void	ft_store_paths(t_var *g, char c, char *str);
+void	check_paths(t_var *g);
 
 // Functions used to check & store Map :
-void	check_map_validity(t_var *g, char **map, int nb_line);
+
+void	check_map_validity(t_var *g, char **map);
 int		count_map_lines(t_game *head);
-int		ft_check_map(t_var *g, t_game *head);
+void    ft_check_map(t_var *g, t_game *head);
 void	save_check_map_validity(t_var *g, t_game *head, int count);
 void	ft_skip_space(char *str);
 int		ch(char c);
@@ -105,5 +102,15 @@ char	*get_remaining_part(char *old_buffer);
 char	*ft_read(int fd, char *buffer);
 char	*get_next_line(int fd);
 int     ft_check_newline(char *s);
+
+// Mini_map Functions :
+
+void    init_and_create_window(t_var *g);
+int     is_player(char c);
+int     get_color(char c);
+void    ft_put(int color, int x, int y);
+void    ft_show_minimap(t_var *g);
+
+
 
 #endif
