39ff2bda43877071a8e7ed60cb5ca7e230a7db2c
a <HK-MOTO>
2022-11-03 12:14:19 +0100

    feat :normalize angles & add loop for rendering ✨

--- src/main_function/cub3D.c
+++ src/main_function/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 05:20:47 by zaabou            #+#    #+#             */
-/*   Updated: 2022/10/29 22:24:49 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/11/03 11:39:29 by zaabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ void	initialize_data(t_var *g)
 
 void	init_player_info(t_player *pl)
 {
-	pl->angle = 180 * (PI / 180);
-	pl->m_speed = 3;
-	pl->r_speed = 3 * (PI / 180);
+	pl->angle = (3 * PI) / 2 + (30 * (PI / 180));
+	pl->m_speed = 5;
+	pl->r_speed = 5 * (PI / 180);
 }
 
 int	main(int ac, char **av)
@@ -46,10 +46,9 @@ int	main(int ac, char **av)
 		start_processing(av[1], &g);
 		init_player_info(g.player);
 		ft_mlx(&g);
-		ft_rendering(&g);
 		creat_rays_list(&g);
-		raycasting(&g);
-		mlx_put_image_to_window((g.mlx_ptr)->ptr_mlx, (g.mlx_ptr)->ptr_win, (g.mlx_ptr)->ptr_img, 0, 0);
+		mlx_hook(g.mlx_ptr->ptr_win, 2, 0, ft_press_key, &g);
+		mlx_loop_hook(g.mlx_ptr->ptr_mlx, ft_rendering, &g);
 		mlx_loop(g.mlx_ptr->ptr_mlx);
 	}
 	return (0);
--- src/rendring/rendering.c
+++ src/rendring/rendering.c
@@ -6,76 +6,64 @@
 /*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 12:49:51 by zaabou            #+#    #+#             */
-/*   Updated: 2022/10/29 03:10:54 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/11/03 11:35:06 by zaabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cub3d.h>
 
-# include <cub3d.h>
-# include <mlx.h>
-void	ft_rendering(t_var *g)
+void	ft_mini_map(t_var *g)
 {
+	put_elements(g);
+	ft_put_player(g);
+}
+
+void	normalize_angle(double *angle)
+{
+	if (*angle > 2 * PI)
+		*angle -= 2 * PI;
+	else if (*angle < 0)
+		*angle += 2 * PI;
+}
+
+int	ft_rendering(t_var *g)
+{
+	if (g->player->turn == 1)
+		g->player->angle += g->player->r_speed;
+	if (g->player->turn == -1)
+		g->player->angle -= g->player->r_speed;
+	ft_release_key(g);
+	normalize_angle(&(g->player->angle));
+	fill_img(g);
+	raycasting(g);
 	ft_mini_map(g);
+	mlx_put_image_to_window((g->mlx_ptr)->ptr_mlx,
+		(g->mlx_ptr)->ptr_win, (g->mlx_ptr)->ptr_img, 0, 0);
+	return (1);
 }
 
 void	ft_draw_line(t_var *g, int x_e, int y_e)
 {
-	// int	dx;
-	// int	dy;
-	// int	dp;
-	// int	x_s;
-	// int	y_s;
+	float	steps;
+	int		dx;
+	int		dy;
+	int		i;
+	float	increment_x;
+	float	increment_y;
+	float	x;
+	float	y;
 
-	// x_s = g->player->pos_x;
-	// y_s = g->player->pos_y;
-	// dx = abs(x_e - x_s);
-	// dy = abs(y_e - y_s);
-	// dp = (2 * dy) - dx;
-	// while (1)
-	// {
-	// 	if (x_s == x_e && y_s == y_e)
-	// 		break ;
-	// 	if (dp < 0)
-	// 	{
-	// 		dp = dp + (2 * dy);
-	// 		if (x_s < x_e)
-	// 			x_s += 1;
-	// 		else if (x_s > x_e)
-	// 			x_s -= 1;
-	// 	}
-	// 	else
-	// 	{
-	// 		dp = dp + (2 * dy) - (2 * dx);
-	// 		if (x_s < x_e)
-	// 			x_s += 1;
-	// 		else if (x_s > x_e)
-	// 			x_s -= 1;
-	// 		if (y_s < y_e) 
-	// 			y_s += 1;
-	// 		else if (y_s > y_e)
-	// 			y_s -= 1;
-	// 	}
-	// 	ft_colorize_pixel(g, x_s, y_s, rays_color);
-	float steps;
-	int dx;
-	int dy;
-	float increment_x;
-	float increment_y;
-	float x;
-	float y;
-
-	x = g->player->pos_x;
-	y = g->player->pos_y;
-	int i = 0;
-
-	dx = x_e - g->player->pos_x;
-	dy = y_e - g->player->pos_y;
+	x = g->player->pos_x + distance(g, 'x');
+	y = g->player->pos_y + distance(g, 'y');
+	dx = x_e - (g->player->pos_x + distance(g, 'x'));
+	dy = y_e - (g->player->pos_y + distance(g, 'y'));
 	if (abs(dx) >= abs(dy))
 		steps = abs(dx);
 	else
 		steps = abs(dy);
 	increment_x = dx / steps;
 	increment_y = dy / steps;
+	i = 0;
 	while (i <= steps)
 	{
 		ft_colorize_pixel(g, round(x), round(y), rays_color);
