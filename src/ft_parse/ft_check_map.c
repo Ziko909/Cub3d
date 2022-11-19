81ac8a829fb7475df74c1eb6912e1cdeab2b54dd
Rel-hach <rachidelhachimi6@gmail.com>
2022-11-20 00:03:22 +0100

    fix: handle empty map & consecutive \n error.

--- src/ft_parse/ft_check_map.c
+++ src/ft_parse/ft_check_map.c
@@ -3,20 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:06:31 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/02 22:12:11 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/11/10 21:33:51 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/cub3d.h"
-
-bool	valid_index(int j, char *prev_line, char *next_line)
-{
-	return (prev_line && j <= (int) ft_strlen(prev_line)
-		&& next_line && j <= (int) ft_strlen(next_line));
-}
+#include <cub3d.h>
 
 bool	check_circumference(int j, char *line, char *prev_line, char *next_line)
 {
@@ -48,15 +42,16 @@ void	check_map_validity(t_var *g, char **map)
 				|| map[i][j] == 'S')
 			{
 				if (i == 0 || !valid_index(j, map[i - 1], map[i + 1]))
-					ft_put_error("Map must be surrounded by walls here");
-				if (j == 0 || !check_circumference(j, map[i], map[i - 1], map[i + 1]))
-					ft_put_error("Map must be surrounded by walls");
+					ft_put_error(g, "Map must be surrounded by walls here");
+				if (j == 0 || !check_circumference(j, map[i],
+						map[i - 1], map[i + 1]))
+					ft_put_error(g, "Map must be surrounded by walls");
 				save_player_position(g, map[i][j], j, i);
 			}
 		}
 	}
 	if (g->data->nb_player != 1)
-		ft_put_error("Wrong number of players");
+		ft_put_error(g, "Wrong number of players");
 }
 
 void	store_check_map_validity(t_var *g, t_game *head, int count)
@@ -68,18 +63,22 @@ void	store_check_map_validity(t_var *g, t_game *head, int count)
 	g->data->large_line = 0;
 	i = -1;
 	g->data->map = ft_calloc(sizeof (char *), count + 1);
+	if (!g->data->map)
+		ft_put_error(g, "Allocation failed");
 	while (temp && ft_strncmp(temp->line, "\n", 1) != 0)
 	{
 		if (g->data->large_line < (int) ft_strlen(temp->line))
 			g->data->large_line = (int) ft_strlen(temp->line);
-		g->data->map[++i] = ft_strdup(temp->line);
+		g->data->map[++i] = ft_strtrim(temp->line, "\n");
+		if (!g->data->map[i])
+			ft_put_error(g, "Allocation failed");
 		temp = temp->next;
 	}
 	g->data->map_lines = count;
 	check_map_validity(g, g->data->map);
 }
 
-int	count_map_lines(t_game *head)
+int	count_map_lines(t_game *head, t_var *g)
 {
 	int		count;
 	t_game	*temp;
@@ -91,6 +90,10 @@ int	count_map_lines(t_game *head)
 		temp = temp->next;
 		count++;
 	}
+	while (temp && ft_strncmp(temp->line, "\n", 1) == 0)
+		temp = temp->next;
+	if (temp)
+		ft_put_error(g, "Map is invalid");
 	return (count);
 }
 
@@ -103,6 +106,6 @@ void	ft_check_map(t_var *g, t_game *head)
 	count = 0;
 	while (temp->next && ft_strncmp(temp->line, "\n", 1) == 0)
 		temp = temp->next;
-	count = count_map_lines(temp);
+	count = count_map_lines(temp, g);
 	store_check_map_validity(g, temp, count);
 }
